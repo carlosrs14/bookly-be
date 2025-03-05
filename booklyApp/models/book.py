b67bf95f141c291a0421d6508430e4dac8db5526
carlosrs14 <crincones578@gmail.com>
2025-03-05 00:16:06 -0500

    correction on model book make many to many field on geners

--- booklyApp/models/book.py
+++ booklyApp/models/book.py
@@ -3,11 +3,12 @@ from .author import Author
 from .gender import Gender
 
 class Book(models.Model):
-    title = models.TextField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    genre = models.ForeignKey(Gender, on_delete=models.CASCADE, related_name='books')
-    published_year = models.IntegerField(null=True, blank=True)
-
+    title = models.TextField(max_length = 100)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name = 'books')
+    genres = models.ManyToManyField(Gender, related_name = 'books')
+    published_year = models.IntegerField(null = True, blank = True)
+    synopsis = models.TextField(blank = True)
+    
     def __str__(self):
         return self.title
     
